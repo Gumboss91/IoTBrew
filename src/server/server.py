01335d8e826e43b27225cf6e332dcd7b130c9d14
daschaef <daschaef@sch.xx>
2019-10-26 22:49:14 +0200

    urls

--- src/server/server.py
+++ src/server/server.py
@@ -4,7 +4,14 @@ from coapthon.client.helperclient import HelperClient as CoapClient
 
 logging.disable(logging.DEBUG)
 
-path ="sen/readings/temp"
+urls = [
+    "sen/readings/temp",
+    "sen/readings/batt",
+    "sen/readings/press",
+    "sen/readings/hum",
+    "sen/readings/light"
+]
+path =
 COAP_PORT = 5683
 
 LOCALPORT = 3001
@@ -16,12 +23,12 @@ while True:
     message, address = server_socket.recvfrom(1024)
     print("UDP received, starting coap", address[0])
     caophost = address[0]
-    coapclient = CoapClient(server=(caophost, COAP_PORT))
-    response = coapclient.get(path, timeout=20)
 
-    if(response):
-        print("Received", response.pretty_print())
-    else:
-        print("Coap Timeout")
-
-    coapclient.stop()
+    for url in urls:
+        coapclient = CoapClient(server=(caophost, COAP_PORT))
+        response = coapclient.get(url, timeout=20)
+        if(response):
+            print("Received", response.pretty_print())
+        else:
+            print("Coap Timeout")
+        coapclient.stop()
