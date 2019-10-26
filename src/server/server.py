f1899249b8696a05d3ed6c5fdedb566ea08bc0d4
daschaef <daschaef@sch.xx>
2019-10-26 21:26:13 +0200

    change to ipv6

--- src/server/server.py
+++ src/server/server.py
@@ -2,7 +2,7 @@ import socket
 
 LOCALPORT = 3001
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
 server_socket.bind(('', LOCALPORT))
 
 while True:
