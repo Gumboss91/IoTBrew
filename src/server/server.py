f2eeee26d5653562039686f9113edb962502af45
daschaef <daschaef@sch.xx>
2019-10-26 21:22:44 +0200

    fix

--- src/server/server.py
+++ src/server/server.py
@@ -6,6 +6,5 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind(('', LOCALPORT))
 
 while True:
-    rand = random.randint(0, 10)
     message, address = server_socket.recvfrom(1024)
     print("Received", message, address)
