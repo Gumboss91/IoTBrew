00d5029135b0b5233f8a72a21fcff31f763b116c
daschaef <daschaef@sch.xx>
2019-10-28 22:23:23 +0100

    include mqtt

--- src/server/server.py
+++ src/server/server.py
@@ -1,6 +1,8 @@
 import socket
 import logging
+import json
 from coapthon.client.helperclient import HelperClient as CoapClient
+import paho.mqtt.client as mqtt
 
 logging.disable(logging.DEBUG)
 
@@ -13,13 +15,33 @@ urls = [
 ]
 
 COAP_PORT = 5683
+LOCAL_PORT = 3001
 
-LOCALPORT = 3001
+MQTT_BROCKER = "127.0.0.1"
 
+mqtt_connected = False
+
+# MQTT functions
+def mqtt_on_connect(client, userdata, flags, rc):
+    mqtt_connected = True
+
+def mqtt_on_disconnect(client, userdata, flags, rc):
+    mqtt_connected = False
+
+# UDP
 server_socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-server_socket.bind(('', LOCALPORT))
+server_socket.bind(('', LOCAL_PORT))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+
+client.loop_start()
 
 while True:
+    if not mqtt_connected:
+        client.connect(MQTT_BROCKER)
+
     message, address = server_socket.recvfrom(1024)
     print("UDP received, starting coap", address[0])
     caophost = address[0]
@@ -29,6 +51,10 @@ while True:
         response = coapclient.get(url, timeout=20)
         if(response):
             print("Received", response.pretty_print())
+            if mqtt_connected:
+                elem = response.payload
+                client.publish("6lopawan/sensor/" + caophost + "/" + url, elem)
+                print("Publish:", elem)
         else:
             print("Coap Timeout")
         coapclient.stop()
