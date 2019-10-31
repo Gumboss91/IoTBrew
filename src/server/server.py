32b78bd55f6728736c9b8f70c6cd3b7a34523944
daschaef <daschaef@sch.xx>
2019-10-31 23:20:16 +0100

    debug

--- src/server/server.py
+++ src/server/server.py
@@ -53,7 +53,7 @@ def mqtt_on_disconnect(client, userdata, flags, rc):
 ttl = struct.pack('b', 100)
 server_socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
 server_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
-server_socket.sendto(bytes("ffff\n\n"), (IP_MULTICAST, PORT_DISCOVERY))
+server_socket.sendto(bytes("ffff\n\n", "utf-8"), (IP_MULTICAST, PORT_DISCOVERY))
 while True:
     #if not mqtt_connected:
     #    client.connect(MQTT_BROCKER)
